030de9bcdb0b973e2944394adbb850d1cce8204d
omarelnahs <omarelnahas1141@gmail.com>
2025-01-06 16:55:36 +0200

    CRUD operations

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,7 +4,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 let cashed = (global as any).mongoose || { conn: null, promise: null };
 
-export const connnectToDatabase = async () => {
+export const connectToDatabase = async () => {
     if (cashed.conn) return cashed.conn;
     
     if(!MONGODB_URI) 
